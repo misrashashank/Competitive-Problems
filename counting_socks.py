7ffeed52d007334207af9e76789036699fe30d8d
Shashank Misra <misra.shashank1@gmail.com>
2018-12-17 15:10:03 -0800

    Add: Counting Valleys and Counting Socks problems.

---
+++ counting_socks.py
@@ -0,0 +1,21 @@
+# Given an array of integers representing the color of each sock, determine
+# how many pairs of socks with matching colors there are.
+# Function must return an integer representing the number of matching pairs
+# of socks that are available.
+
+
+def sockMerchant(num, op_array):
+    pairs = {}
+    count = 0
+    for item in op_array:
+        if item in pairs:
+            pairs[item] += 1
+        else:
+            pairs[item] = 1
+    for item in pairs.items():
+        count = count + int(item[1]/2)
+    print(count)
+
+
+if __name__ == '__main__':
+    sockMerchant(10, [1, 2, 3, 1, 3, 2, 2, 1, 2, 3])
---
+++ counting_valleys.py
@@ -0,0 +1,33 @@
+# Gary tracks his hikes meticulously. During his last hike he took n steps.
+# For every step he took, he noted if it was an uphill, U,
+# or a downhill, D step.
+# Gary's hikes start and end at sea level and each step up or down represents
+# a 1 unit change in altitude.
+# A mountain is a sequence of consecutive steps above sea level,
+# starting with a step up from sea level and end with a step down to sea level.
+# A valley is a sequence of consecutive steps below sea level,
+# starting with a step down from sea level and end with a step up to sea level.
+
+
+def countingValleys(num, seq):
+    up = down = valley = 0
+    trek = []
+    for item in list(seq):
+        if item == "D":
+            down += 1
+            trek.append(item)
+        else:
+            up += 1
+            trek.append(item)
+        if down == up and trek[0] == "Down":
+            valley += 1
+            down = up = 0
+            trek = []
+        elif down == up and trek[0] == "U":
+            down = up = 0
+            trek = []
+    print("Valley: {}".format(valley))
+
+
+if __name__ == '__main__':
+    countingValleys(8, "UDDDUDUU")
